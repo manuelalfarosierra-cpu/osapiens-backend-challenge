b2e3a7e15a44d1cc644cf6ad68eea7568cd45f8e
Leopold Wieser <162152877+Leopold-Wieser@users.noreply.github.com>
2024-12-10 13:37:59 +0000

    initial commit

---
+++ src/routes/defaultRoute.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import fs from "fs";
+import path from "path";
+import { marked } from "marked";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const readmePath = path.join(__dirname, "../..", "README.md");
+  fs.readFile(readmePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading README.md:", err);
+      return res.status(500).send("Error loading README.md");
+    }
+    const htmlContent = marked(data);
+    res.setHeader("Content-Type", "text/html");
+    res.send(htmlContent);
+  });
+});
+
+export default router;
