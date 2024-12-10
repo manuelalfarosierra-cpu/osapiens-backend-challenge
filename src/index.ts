50162ae3d5912ae751d4e68f8166fca820ce436a
CodeSandbox <support@codesandbox.io>
2024-12-10 12:45:36 +0000

    Sandbox snapshot commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import express from 'express';
+import analysisRoutes from './routes/analysisRoutes';
+import { taskWorker } from './workers/taskWorker';
+import { AppDataSource } from './data-source'; // Import the DataSource instance
+
+const app = express();
+app.use(express.json());
+app.use('/analysis', analysisRoutes);
+
+AppDataSource.initialize()
+    .then(() => {
+        // Start the worker after successful DB connection
+        taskWorker();
+
+        app.listen(3000, () => {
+            console.log('Server is running at http://localhost:3000');
+        });
+    })
+    .catch((error) => console.log(error));
